d3d08388af566d3201af71340825b2ae1dbda5f8
Tom Newman <T.Newman1@newcastle.ac.uk>
2023-02-14 14:20:46 +0000

    Add project files.

---
+++ Apache Logs/ApacheLogComponents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LogAnalysisTool.Apache_Logs
+{
+    internal class ApacheLogComponents : ILogTypeInfo
+    {
+    
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.UI;
+
+namespace LogAnalysisTool
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
