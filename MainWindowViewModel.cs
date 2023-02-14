c35722fed861dbe99f51c2a80ac91b4e2600ce73
Tom Newman <T.Newman1@newcastle.ac.uk>
2023-02-14 15:50:46 +0000

    Added ability to open file dialog and select a log file to be processed, updating the textbox with the name of the selected file.

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -12,6 +12,36 @@ namespace LogAnalysisTool
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        private DelegateCommand _browseCommand;
+        
+        private string _fileName;
+        public IOpenFileDialogService OpenFileDialogService => GetService<IOpenFileDialogService>();
+        public ICommand BrowseCommand => _browseCommand ??= new DelegateCommand(FileBrowser);
 
+        public MainWindowViewModel()
+        {
+            _browseCommand = new DelegateCommand(FileBrowser);
+        }
+
+        public string FileName
+        {
+            get => _fileName;
+            set
+            {
+                if (_fileName == value) 
+                {
+                    return; 
+                }
+
+                _fileName = value;
+                RaisePropertyChanged(nameof(FileName));
+            }
+            
+        }
+        private void FileBrowser()
+        {
+            _ = OpenFileDialogService.ShowDialog();
+            FileName = OpenFileDialogService.File.GetFullName();
+        }
     }
 }
