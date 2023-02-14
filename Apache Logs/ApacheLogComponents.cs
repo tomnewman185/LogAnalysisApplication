226e6c095c3d1df7de6b06e03cbf4eb9465b99db
Tom Newman <T.Newman1@newcastle.ac.uk>
2023-02-14 15:14:07 +0000

    Added support for Apache log type including implementation of regex to break each log entry up into its different components.

--- Apache Logs/ApacheLogComponents.cs
+++ Apache Logs/ApacheLogComponents.cs
@@ -9,6 +9,36 @@ namespace LogAnalysisTool.Apache_Logs
 {
     internal class ApacheLogComponents : ILogTypeInfo
     {
+        public ApacheLogComponents()
+        {
 
+        }
+
+        //Interface properties for log type Apache
+        public string Name => "Apache Log Type Components";
+
+        //Regex breaks up each log entry into its different components
+        public string RegularExpression => $@"^""(?<{Regex.Escape(RegexComponentGroups.RemoteHost)}>\S*)\s+(?<{Regex.Escape(RegexComponentGroups.RFC931)}>\S+)\s+(?<{Regex.Escape(RegexComponentGroups.AuthUser)}>\S+).*\[(?<{Regex.Escape(RegexComponentGroups.Date)}>.*)\]\s""(?<{Regex.Escape(RegexComponentGroups.Request)}>[^""]*)""\s(?<{Regex.Escape(RegexComponentGroups.Status)}>\S*)\s(?<{Regex.Escape(RegexComponentGroups.Bytes)}>\S*)\s""(?<{Regex.Escape(RegexComponentGroups.Referer)}>[^""]*)""\s""(?<{Regex.Escape(RegexComponentGroups.UserAgent)}>[^""]*)""""$";
+
+        //Defines the different component groups that are present within each log entry
+        internal class RegexComponentGroups
+        {
+            public static string RemoteHost => "1";
+            public static string RFC931 => "2";
+
+            public static string AuthUser => "3";
+
+            public static string Date => "4";
+
+            public static string Request => "5";
+
+            public static string Status => "6";
+
+            public static string Bytes => "7";
+
+            public static string Referer => "8";
+
+            public static string UserAgent => "9";
+        }
     }
 }
